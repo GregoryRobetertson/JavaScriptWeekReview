6972e113013eaeb013aa2cf088d0293a4c674e40
GregoryRobertson <geefromla@gmail.com>
2023-12-01 21:11:29 -0600

    create index.html and script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+ const message = document.querySelector('#message');
+ const ul = document.querySelector('ul');
+ const form = document.querySelector('form');
+ const color = ['red', 'blue', 'green', 'yellow', 'purple'];
+const nums = [1, 2, 3, 4 ,5];
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const li = document.createElement('li');
+    const name = e.target.name.value;
+    const age = e.target.age.value;
+    li.innerText =
+    `Name: ${name}
+     Age:  ${age}`;
+     ul.appendChild(li);
+    });
+
+for(let i = 0; i < color.length; i++) {
+    const p = document.createElement('p');
+    p.innerText = color[i];
+    message.appendChild(p);
+}
+
+let counter = 0;
+while (counter < 5) {
+    const p = document.createElement('p');
+    p.innerText = `count: ${counter + 1}`;
+    message.appendChild(p);
+    counter++;
+}
+
+const newNums = nums.map((number) => number * 2);
+console.log(newNums);
+
+function forEachMethod() {
+    odds.foreach((number) => {
+        const li = document.createElement('li');
+        li.innerText = number;
+        ul.appendChild(li);
+    });
+}
+
+forEachMethod();
+
+const myArray1 = [1, 2, 3];
+const myArray2 = myArray1;
+myArray1.push('New Value');
+console.log(myArray2);
+
+let myNum1 = 5;
+let myNum2 = myNum1;
+myNum1 = 10;
+
+console.log(myNum2);
+
